c9b88854a2379a2fd52fc5465e57fad419e2cb26
Felix <felixochieng5785@gmail.com>
2026-05-26 04:35:08 +0300

    Fix URL routing - properly include core.urls for root path

--- config/urls.py
+++ config/urls.py
@@ -1,29 +1,23 @@
 """
 URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+# Custom error handler
 handler403 = 'core.views.access_denied_view'
 
 urlpatterns = [
+    # Admin panel
     path('admin/', admin.site.urls),
+    
+    # Include ALL URLs from core/urls.py (including the root '' path)
+    # The order matters: include core.urls FIRST so it catches the root URL
     path('', include('core.urls')),
+    
+    # Authentication URLs (these will be at /accounts/login/ etc.)
     path('accounts/login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
